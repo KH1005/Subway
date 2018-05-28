8a7838a822af7c560b4fb9135ac728f6d32cbe51
sbk620 <sbk620@naver.com>
2018-05-28 18:26:53 +0900

    no message

---
+++ src/member/loginForm.java
@@ -0,0 +1,30 @@
+package member;
+import java.io.Reader;
+import java.util.Map;
+import org.apache.struts2.interceptor.SessionAware;
+import com.ibatis.common.resources.Resources;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+
+
+public class loginForm extends ActionSupport {
+
+
+	
+	public String execute() throws Exception{
+		return SUCCESS;
+	}
+	
+	
+	
+	
+}
+
+	
+	
+	
+	
+	
